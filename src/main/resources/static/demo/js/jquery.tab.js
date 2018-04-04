4f70c33b16b65dd07c5503e1c99b5c94fc248a22
issunlei <36222670+issunlei@users.noreply.github.com>
2018-04-04 14:24:16 +0800

    功能提交

--- src/main/resources/static/demo/js/jquery.tab.js
+++ src/main/resources/static/demo/js/jquery.tab.js
@@ -1,7 +1,7 @@
 (function ($) {
     var itemD = $('<li class="nav-item"><a class="nav-link" data-toggle="tab"></a></li>'),
         paneD = $('<div class="tab-pane fade" style="padding:15px;">'),
-        closeD = $('<button type="button" class="close" aria-hidden="true" style="padding-right:3px;" onmouseover="this.style.opacity=0.2" onmouseout="this.style.opacity=0">&times;</button>');
+        closeD = $('<button type="button" class="close" aria-hidden="true" style="padding-right:3px;">&times;</button>');
 
     function tabs(conf) {
         this.confs = {};
@@ -22,6 +22,7 @@
                 conf.tabs[i].activeIndex = conf.activeIndex;
                 this.add(conf.tabs[i]);
             }
+
         }
     };
 
@@ -37,7 +38,15 @@
             }
 
             var id = api.util.generateId(), item = itemD.clone(), tabs = this, a = item.find('>a').attr('href', '#' + id).text(title), pane = paneD.clone().attr('id', id);
+            item.css('width', tabConf.width);
             if (tabConf.closable) {
+                a.css('padding-right', '16px');
+                item.prepend(closeD.clone().click(function () {
+                    tabs.close(title);
+                }));
+            }
+
+            if (conf.closable) {
                 a.css('padding-right', '16px');
                 item.prepend(closeD.clone().click(function () {
                     tabs.close(title);
@@ -55,19 +64,22 @@
                 event.stopPropagation();
                 that.load(tabConf);
             });
+
             //判断是否需要延迟加载标签
-            if (tabConf.lazy) {
-                //活动tab需要加载内容
-                if (tabConf.activeIndex == tabConf.index) {
-                    item.trigger("click");
-                }
-            } else {
-                item.trigger("click");
+            if (!tabConf.lazy) {
+                that.load(tabConf);
+            }
+
+            //活动tab需要加载内容和显示
+            if (tabConf.activeIndex == tabConf.index) {
+                that.load(tabConf);
+                that.show(title);
             }
 
             if (tabConf.disabled) {
                 this.disable(title);
             }
+            return this;
         },
         close: function (title) {
             var a = this.nav.find('a:contains(' + title + ')'), id = a.attr('href'), p = a.parent();
@@ -77,15 +89,19 @@
             }
             this.content.find('>div' + id).remove();
             p.remove();
+            return this;
         },
         show: function (title) {
             this.nav.find('a:contains(' + title + ')').tab('show');
+            return this;
         },
         hide: function (title) {
             this.nav.find('a:contains(' + title + ')').parent().css('display', 'none');
+            return this;
         },
         display: function (title) {
             this.nav.find('a:contains(' + title + ')').parent().css('display', '');
+            return this;
         },
         contains: function (title) {
             return this.nav.find('a:contains(' + title + ')').length;
@@ -95,6 +111,7 @@
                 this.confs[title].panel.loaded = false;
             }
             this.load(this.confs[title]);
+            return this;
         },
         load: function (tabConf) {
             var title = tabConf.title, href = tabConf.href, pane = tabConf.panel, content = tabConf.content;
@@ -111,33 +128,38 @@
                 });
                 pane.loaded = true;
             }
+            return this;
         },
         tab: function (title) {
             var jq = this.content.find('>div' + this.nav.find('a:contains(' + title + ')').attr('href'));
             jq.conf = this.confs[title];
-            return jq;
+            return this;
         },
         activeTab: function () {
             var a = this.nav.find('>li.active a'), jq = this.content.find('>div' + a.attr('href'));
             jq.conf = this.confs[a.text()];
-            return jq;
+            return this;
         },
         destroy: function () {
             this.nav.remove();
             this.content.remove();
+            return this;
         },
         disable: function (title) {
             this.nav.find('a:contains(' + title + ')').parent().addClass('disabled').on('click.disabled', function () {
                 return false;
             });
+            return this;
         },
         enable: function (title) {
             this.nav.find('a:contains(' + title + ')').parent().removeClass('disabled').unbind('click.disabled');
+            return this;
         }
     };
 
     var tabDefaults = {
         title: '',
+        width: '8%',
         content: '',
         href: '',
         lazy: true,
