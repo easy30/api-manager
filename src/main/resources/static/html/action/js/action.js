d7a475eaf1474144a7b615cde93b4d81fc855b71
zyz259 <1342082842@qq.com>
2018-04-09 18:00:10 +0800

    页面修改

--- src/main/resources/static/html/action/js/action.js
+++ src/main/resources/static/html/action/js/action.js
@@ -32,55 +32,46 @@ var actionTableOptions = {
     ],
     rowButtons: [
         {type: 'view', text: '查看', icon: 'glyphicon glyphicon-eye-open', fn: function (param) {
-            var conf = {
-                container: '#container',
-                url: 'html/action/actionTab.html',
-                content: "",
-                async: false,
-                preLoad: function () {},
-                loaded: function () {
-                    $('#headButton button:last').css('display','none');
-                    var conf = {
-                        container: '#tabs',
-                        tabs: [{
-                            title: '基本信息',
-                            width: '10%',
-                            href: 'html/action/actionInfo1.html',
-                            loaded: function () {
-                                $.ajax({
-                                    type: 'get',
-                                    url: '/apimanager/action/findById',
-                                    data:{id: param.id},
-                                    dataType: "json",
-                                    contentType: 'json',
-                                    success: function(result){
-                                        var data = result['data'];
-                                        console.log(data);
-                                        var actionInfoFormOptions={
-                                            container:'#actionInfoForm'
-                                        };
-                                        var actionInfoFormObject =  api.ui.form(actionInfoFormOptions);
-                                        console.log(actionInfoFormObject.toJson());
-                                        actionInfoFormObject.giveVal({
-                                            requestUrl: data['requestUrl'],
-                                            actionName:data['actionName'],
-                                            actionDesc: data['actionDesc']
-                                        })
-                                        actionInfoFormObject.disable();
-                                    }
-                                })
-                            }
-                        }, {
-                            title: '接口参数',
-                            width: '10%',
-                            href: 'html/action/actionParam.html'
-                        }]
-                    };
-                    var tabs = api.ui.tabs(conf);
-                }
-            }
-            api.ui.load(conf);
-
+                var conf = {
+                    container: '#container',
+                    url: 'html/action/actionTab.html',
+                    content: "",
+                    async: false,
+                    preLoad: function () {},
+                    loaded: function () {
+                        $('#headButton button:last').css('display','none');
+                        $('input[name=id]').val(param.id);
+                        api.util.loadScript('html/action/js/actionTab.js', function () {
+                            api.ui.tabs(actionTabConf);
+                        });
+                    }
+                };
+                api.ui.load(conf);
+                var actionInfoFormOptions={
+                    container:'#tabs'
+                };
+                var actionInfoFormObject =  api.ui.form(actionInfoFormOptions);
+                $('#headButton button:first').on('click', function () {
+                    actionInfoFormObject.enable();
+                    $('#headButton button:first').css('display','none');
+                    $('#headButton button:last').css('display','');
+                });
+                //保存
+                $('#headButton button:last').on('click', function () {
+                    $.ajax({
+                        type: 'post',
+                        url: 'apimanager/action/update',
+                        dataType: 'json',
+                        data : JSON.stringify(actionInfoFormObject.toJson()),
+                        contentType : 'application/json;charset=utf-8',
+                        success: function (data) {
+                            //不跳转到列表
+                            actionInfoFormObject.disable();
+                            $('#headButton button:first').css('display','');
+                            $('#headButton button:last').css('display','none');
+                        }
+                    });
+                });
             }
         },
         {type: 'delete', text: '删除', url: '/apimanager/action/delete'}
@@ -93,15 +84,89 @@ var actionTableOptions = {
                     url: 'html/action/actionTab.html',
                     content: "",
                     async: false,
-                    preLoad: function (content) {
-
-                    },
+                    preLoad: function () {},
                     loaded: function () {
                         $('#headButton button:first').css('display','none');
-
+                        api.util.loadScript('html/action/js/actionTab.js', function () {
+                            api.ui.tabs(actionTabConf);
+                        });
                     }
-                }
+                };
                 api.ui.load(conf);
+                var actionInfoFormOptions={
+                    container:'#tabs'
+                };
+                var actionInfoFormObject =  api.ui.form(actionInfoFormOptions);
+                $('#headButton button:first').on('click', function () {
+                    actionInfoFormObject.enable();
+                    $('#headButton button:first').css('display','none');
+                    $('#headButton button:last').css('display','');
+                });
+                //保存
+                $('#headButton button:last').on('click', function () {
+                    $.ajax({
+                        type: 'post',
+                        url: '/apimanager/action/add',
+                        dataType: 'json',
+                        data : JSON.stringify(actionInfoFormObject.toJson()),
+                        contentType : 'application/json;charset=utf-8',
+                        success: function (data) {
+                            //跳转到action列表
+                            var conf = {
+                                container: '#container',
+                                url: 'html/action/action.html',
+                                content: "",
+                                async: false,
+                                preLoad: function () {
+                                    $("#depart").empty();
+                                    $("#depart").append("<li class=\"breadcrumb-item\"><a href=\"javasript:void(0)\" onclick=\"departmentClick()\">Department</a></li>");
+                                    $("#depart").append("<li class=\"breadcrumb-item\"><a href=\"javasript:void(0)\" onclick=\"projectClick1()\">Project</a></li>");
+                                    $("#depart").append("<li class=\"breadcrumb-item\"><a href=\"javasript:void(0)\" onclick=\"moduleClick1()\">Module</a></li>");
+                                    $("#depart").append("<li class=\"breadcrumb-item\"><a href=\"javasript:void(0)\" onclick=\"actionClick1()\">Action</a></li>");
+                                },
+                                loaded: function () {
+                                    var depOptions = {
+                                        selector: '[name=depId]',
+                                        optionField: {value: 'id', text: 'depName'},
+                                        width: '70%',
+                                        url: '/apimanager/department/list',
+                                        change: function (e, p) {
+                                            projectSelect.clear();
+                                            var params = {};
+                                            params['depId']=e.target.value;
+                                            projectSelect.load(params);
+                                        }
+                                    };
+                                    var projectOptions = {
+                                        selector: '[name=projectId]',
+                                        optionField: {value: 'id', text: 'projectName'},
+                                        width: '70%',
+                                        url: '/apimanager/project/list',
+                                        change: function (e, p) {
+                                            moduleSelect.clear();
+                                            var params = {};
+                                            params['projectId']=e.target.value;
+                                            moduleSelect.load(params);
+                                        }
+                                    };
+                                    var moduleOptions = {
+                                        selector: '[name=moduleId]',
+                                        optionField: {value: 'id', text: 'moduleName'},
+                                        width: '70%',
+                                        url: '/apimanager/module/list'
+                                    };
+                                    var projectSelect = api.ui.chosenSelect(projectOptions);
+                                    var moduleSelect = api.ui.chosenSelect(moduleOptions);
+                                    api.ui.chosenSelect(depOptions);
+                                    api.util.loadScript("html/action/js/action.js" ,function () {
+                                        api.ui.editTable(actionTableOptions);
+                                    });
+                                }
+                            }
+                            api.ui.load(conf);
+                        }
+                    });
+                });
             }
         }
     ],
