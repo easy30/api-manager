6d9dfc04681f3371824aac07c4cb9e8302aa6f4b
issunlei <issunlei@sina.cn>
2018-04-12 22:54:46 +0800

    功能提交

--- src/main/resources/static/common/js/login.js
+++ src/main/resources/static/common/js/login.js
@@ -32,3 +32,12 @@ $(document).keydown(function (event) {
         return false;
     }
 });
+
+//能获取到[id=container]节点，说明login.html被嵌套在index.html页面了，这是登录失效重定向的结果
+if($('#container').length){
+    // 打开新窗口
+    window.open('/login.html');
+} else {
+    // 关闭opener窗口
+    window.opener && window.opener.close();
+}
