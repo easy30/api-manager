3518471e4412a38fcf27966856e289b9d4e47a1d
issunlei <36222670+issunlei@users.noreply.github.com>
2018-04-20 13:42:01 +0800

    功能提交

--- src/main/java/com/cehome/apimanager/model/po/AmAction.java
+++ src/main/java/com/cehome/apimanager/model/po/AmAction.java
@@ -5,215 +5,225 @@ import java.util.Date;
 
 /**
  * 接口
- * 
- * @author sunlei
  *
+ * @author sunlei
  */
 public class AmAction implements Serializable {
-	private static final long serialVersionUID = 8747150818253864391L;
-	/**
-	 * 接口编号
-	 */
-	private Integer id;
-	/**
-	 * 接口名称
-	 */
-	private String actionName;
-	/**
-	 * 接口描述
-	 */
-	private String actionDesc;
-	/**
-	 * 接口所属模块
-	 */
-	private Integer moduleId;
-	/**
-	 * 接口请求类型
-	 */
-	private Integer requestType;
-	/**
-	 * 接口请求url
-	 */
-	private String requestUrl;
-	/**
-	 * 接口状态
-	 */
-	private Integer status;
-	/**
-	 * 接口请求头定义
-	 */
-	private String requestHeadDefinition;
-	/**
-	 * 接口请求参数定义
-	 */
-	private String requestDefinition;
-	/**
-	 * 接口返回结果定义
-	 */
-	private String responseDefinition;
-	/**
-	 * 接口请求头mock模板定义
-	 */
-	private String requestHeadMock;
-	/**
-	 * 接口请求参数mock模板定义
-	 */
-	private String requestMock;
-	/**
-	 * 接口响应模板定义
-	 */
-	private String responseMock;
-	/**
-	 * 创建人
-	 */
-	private Integer createUser;
-	/**
-	 * 创建时间
-	 */
-	private Date createTime;
-	/**
-	 * 修改人
-	 */
-	private Integer updateUser;
-	/**
-	 * 修改时间
-	 */
-	private Date updateTime;
+    private static final long serialVersionUID = 8747150818253864391L;
+    /**
+     * 接口编号
+     */
+    private Integer id;
+    /**
+     * 接口名称
+     */
+    private String actionName;
+    /**
+     * 接口描述
+     */
+    private String actionDesc;
+    /**
+     * 接口所属模块
+     */
+    private Integer moduleId;
+    /**
+     * 接口请求类型
+     */
+    private Integer requestType;
+    /**
+     * 域名的id
+     */
+    private Integer domainId;
+    /**
+     * 接口请求url
+     */
+    private String requestUrl;
+    /**
+     * 接口状态
+     */
+    private Integer status;
+    /**
+     * 接口请求头定义
+     */
+    private String requestHeadDefinition;
+    /**
+     * 接口请求参数定义
+     */
+    private String requestDefinition;
+    /**
+     * 接口返回结果定义
+     */
+    private String responseDefinition;
+    /**
+     * 接口请求头mock模板定义
+     */
+    private String requestHeadMock;
+    /**
+     * 接口请求参数mock模板定义
+     */
+    private String requestMock;
+    /**
+     * 接口响应模板定义
+     */
+    private String responseMock;
+    /**
+     * 创建人
+     */
+    private Integer createUser;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 修改人
+     */
+    private Integer updateUser;
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getActionName() {
-		return actionName;
-	}
+    public String getActionName() {
+        return actionName;
+    }
 
-	public void setActionName(String actionName) {
-		this.actionName = actionName;
-	}
+    public void setActionName(String actionName) {
+        this.actionName = actionName;
+    }
 
-	public String getActionDesc() {
-		return actionDesc;
-	}
+    public String getActionDesc() {
+        return actionDesc;
+    }
 
-	public void setActionDesc(String actionDesc) {
-		this.actionDesc = actionDesc;
-	}
+    public void setActionDesc(String actionDesc) {
+        this.actionDesc = actionDesc;
+    }
 
-	public Integer getModuleId() {
-		return moduleId;
-	}
+    public Integer getModuleId() {
+        return moduleId;
+    }
 
-	public void setModuleId(Integer moduleId) {
-		this.moduleId = moduleId;
-	}
+    public void setModuleId(Integer moduleId) {
+        this.moduleId = moduleId;
+    }
 
-	public Integer getRequestType() {
-		return requestType;
-	}
+    public Integer getRequestType() {
+        return requestType;
+    }
 
-	public void setRequestType(Integer requestType) {
-		this.requestType = requestType;
-	}
+    public void setRequestType(Integer requestType) {
+        this.requestType = requestType;
+    }
 
-	public String getRequestUrl() {
-		return requestUrl;
-	}
+    public String getRequestUrl() {
+        return requestUrl;
+    }
 
-	public void setRequestUrl(String requestUrl) {
-		this.requestUrl = requestUrl;
-	}
+    public void setRequestUrl(String requestUrl) {
+        this.requestUrl = requestUrl;
+    }
 
-	public Integer getStatus() {
-		return status;
-	}
+    public Integer getStatus() {
+        return status;
+    }
 
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 
-	public String getRequestHeadDefinition() {
-		return requestHeadDefinition;
-	}
+    public String getRequestHeadDefinition() {
+        return requestHeadDefinition;
+    }
 
-	public void setRequestHeadDefinition(String requestHeadDefinition) {
-		this.requestHeadDefinition = requestHeadDefinition;
-	}
+    public void setRequestHeadDefinition(String requestHeadDefinition) {
+        this.requestHeadDefinition = requestHeadDefinition;
+    }
 
-	public String getRequestDefinition() {
-		return requestDefinition;
-	}
+    public String getRequestDefinition() {
+        return requestDefinition;
+    }
 
-	public void setRequestDefinition(String requestDefinition) {
-		this.requestDefinition = requestDefinition;
-	}
+    public void setRequestDefinition(String requestDefinition) {
+        this.requestDefinition = requestDefinition;
+    }
 
-	public String getResponseDefinition() {
-		return responseDefinition;
-	}
+    public String getResponseDefinition() {
+        return responseDefinition;
+    }
 
-	public void setResponseDefinition(String responseDefinition) {
-		this.responseDefinition = responseDefinition;
-	}
+    public void setResponseDefinition(String responseDefinition) {
+        this.responseDefinition = responseDefinition;
+    }
 
-	public String getRequestHeadMock() {
-		return requestHeadMock;
-	}
+    public String getRequestHeadMock() {
+        return requestHeadMock;
+    }
 
-	public void setRequestHeadMock(String requestHeadMock) {
-		this.requestHeadMock = requestHeadMock;
-	}
+    public void setRequestHeadMock(String requestHeadMock) {
+        this.requestHeadMock = requestHeadMock;
+    }
 
-	public String getRequestMock() {
-		return requestMock;
-	}
+    public String getRequestMock() {
+        return requestMock;
+    }
 
-	public void setRequestMock(String requestMock) {
-		this.requestMock = requestMock;
-	}
+    public void setRequestMock(String requestMock) {
+        this.requestMock = requestMock;
+    }
 
-	public String getResponseMock() {
-		return responseMock;
-	}
+    public String getResponseMock() {
+        return responseMock;
+    }
 
-	public void setResponseMock(String responseMock) {
-		this.responseMock = responseMock;
-	}
+    public void setResponseMock(String responseMock) {
+        this.responseMock = responseMock;
+    }
 
-	public Integer getCreateUser() {
-		return createUser;
-	}
+    public Integer getCreateUser() {
+        return createUser;
+    }
 
-	public void setCreateUser(Integer createUser) {
-		this.createUser = createUser;
-	}
+    public void setCreateUser(Integer createUser) {
+        this.createUser = createUser;
+    }
 
-	public Date getCreateTime() {
-		return createTime;
-	}
+    public Date getCreateTime() {
+        return createTime;
+    }
 
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 
-	public Integer getUpdateUser() {
-		return updateUser;
-	}
+    public Integer getUpdateUser() {
+        return updateUser;
+    }
 
-	public void setUpdateUser(Integer updateUser) {
-		this.updateUser = updateUser;
-	}
+    public void setUpdateUser(Integer updateUser) {
+        this.updateUser = updateUser;
+    }
 
-	public Date getUpdateTime() {
-		return updateTime;
-	}
+    public Date getUpdateTime() {
+        return updateTime;
+    }
 
-	public void setUpdateTime(Date updateTime) {
-		this.updateTime = updateTime;
-	}
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 
+    public Integer getDomainId() {
+        return domainId;
+    }
+
+    public void setDomainId(Integer domainId) {
+        this.domainId = domainId;
+    }
 }
