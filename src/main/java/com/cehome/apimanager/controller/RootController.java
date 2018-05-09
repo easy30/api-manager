3356a79b76edb7ca7018229dd20c88382eadad1a
issunlei <36222670+issunlei@users.noreply.github.com>
2018-05-09 16:45:59 +0800

    功能提交

--- src/main/java/com/cehome/apimanager/controller/RootController.java
+++ src/main/java/com/cehome/apimanager/controller/RootController.java
@@ -1,17 +1,25 @@
 package com.cehome.apimanager.controller;
 
+import com.cehome.apimanager.utils.WebUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class RootController {
 
     @RequestMapping("/")
     public String redirect(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
-        return "login.html";
+        HttpSession session = request.getSession();
+        boolean login = WebUtils.isLogin(session);
+        if(login){
+            return "index.html";
+        } else {
+            return "login.html";
+        }
     }
 }
