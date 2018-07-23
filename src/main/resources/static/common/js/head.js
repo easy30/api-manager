54a76ccec47dccfc9d20e2edab876edd9e43e723
zyz259 <1342082842@qq.com>
2018-07-23 18:47:39 +0800

    项目接口统计页面

--- src/main/resources/static/common/js/head.js
+++ src/main/resources/static/common/js/head.js
@@ -802,3 +802,81 @@ function loggerClick() {
     }
     api.ui.load(conf);
 }
+
+function actionCountClick() {
+    // 基于准备好的dom，初始化echarts实例
+    var containerConf = {
+        container: '#container',
+        url: 'html/action/actionCount.html',
+        loaded: function () {
+            $('#depart').parent('.container-fluid').css('display','none');
+            $.ajax({
+                url: api.util.getUrl('apimanager/action/countGroupByProject'),
+                type: 'get',
+                dataType: 'json',
+                success: function (result) {
+                    var datas = result.data;
+                    var xData = [],series = [];
+                    if(datas){
+                        $.each(datas, function (index, data) {
+                            xData.push(data.projectName);
+                            series.push(data.actionCount);
+                        })
+                    }
+                    console.log(xData);
+                    console.log(series);
+                    var myChart = echarts.init(document.getElementById('actionNum'));
+                    // 指定图表的配置项和数据
+                    var option = {
+                        title: {
+                            text: '项目接口数量统计',
+                            x: '38%',
+                            textStyle:{
+                                fontSize: 25
+                            }
+                        },
+                        legend: {
+                            y: '6%',
+                            x: '880px',
+                            data: ['数量'],
+                            color: 'rgb(42,170,227)',
+                            textStyle:{
+                                fontSize: 18
+                            }
+                        },
+                        grid: {
+                            x: '8%',
+                            width:'80%',
+                            top: '20%',
+                            containLabel: true
+                        },
+                        xAxis: {
+                            data: xData,
+                            axisLabel:{
+                                interval: 0,
+                                rotate: -30
+                            }
+                        },
+                        yAxis: {},
+                        series: [{
+                            name: '数量',
+                            type: 'bar',
+                            data: series,
+                            label: {
+                                show: true,
+                                position: 'top',
+                                color: 'black'
+                            },
+                            itemStyle: {
+                                color: 'rgb(42,170,227)'
+                            }
+                        }]
+                    };
+                    // 使用刚指定的配置项和数据显示图表。
+                    myChart.setOption(option);
+                }
+            })
+        }
+    }
+    api.ui.load(containerConf);
+}
